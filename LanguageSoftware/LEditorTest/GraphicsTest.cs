71e4cfe2e1aa73ef3e10d2017fd4b4ac298f9b9b
Jack Martin <jackdmartin14@gmail.com>
2020-10-08 06:26:18 +0100

    LE-1: Added editor Projects

---
+++ LanguageSoftware/LEditorTest/GraphicsTest.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+
+namespace LEditorTest
+{
+    [TestFixture]
+    public class GraphicsTest
+    {
+        [SetUp]
+        public void SetUpDrawing()
+        {
+            
+        }
+        
+        [Test]
+        public void ScriptMethodLinkTest()
+        {
+            
+        }
+        
+        [Test]
+        public void DrawTriangleTest()
+        {
+            
+        }
+        
+        [Test]
+        public void DrawCircleTest()
+        {
+            
+        }
+        
+        [Test]
+        public void DrawRectTest()
+        {
+            
+        }
+        
+        [Test]
+        public void DrawPolygonTest()
+        {
+            
+        }
+        
+        [Test]
+        public void MoveToTest()
+        {
+            
+        }
+        
+        [Test]
+        public void DrawToTest()
+        {
+            
+        }
+        
+        [Test]
+        public void FillTest()
+        {
+            
+        }
+        
+        [Test]
+        public void ResetTest()
+        {
+            
+        }
+    }
+}
